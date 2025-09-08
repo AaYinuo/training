2b9f43e47d2865cd8c60090d8558d91ab7a9ca5b
yinuozhang <send2yinuoz@gmail.com>
2025-09-08 14:03:33 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-const CACHE_NAME = 'fittrack-cache-v1';
+const CACHE_NAME = 'fittrack-cache-v2';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './app.js',
 ];
-self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); });
-self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); });
-self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request))); });
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
